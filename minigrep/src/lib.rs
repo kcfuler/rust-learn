7913501c675c3013be1a3c4eaa882e59829734f7
kcfuler <kcfuler@gmail.com>
2023-11-12 00:17:56 +0800

    feat: add case_insensitive

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -1,5 +1,47 @@
 use std::error::Error;
-use std::fs;
+use std::{env, fs};
+
+pub struct Config {
+    pub query: String,
+    pub file_path: String,
+    pub ignore_case: bool,
+}
+
+// 使用面向对象的方式编程，让数据结构更清晰
+impl Config {
+    // 使用 Result 的方式来处理
+    pub fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+        let ignore_case = env::var("CASE_INSENSITIVE").is_ok();
+
+        Ok(Config {
+            query,
+            file_path,
+            ignore_case,
+        })
+    }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
+
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
+
+    for line in results {
+        println!("{line}");
+    }
+
+    Ok(())
+}
 
 /*
 1. 遍历迭代 contents 的每一行
@@ -18,36 +60,20 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     results
 }
 
-pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.file_path)?;
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    // to_lowercase() 方法返回一个新的 String，而不是修改原来的 String
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
 
-    for line in search(&config.query, &contents) {
-        println!("{line}");
-    }
-
-    Ok(())
-}
-
-pub struct Config {
-    pub query: String,
-    pub file_path: String,
-}
-
-// 使用面向对象的方式编程，让数据结构更清晰
-impl Config {
-    // 使用 Result 的方式来处理
-    pub fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
+    for line in contents.lines() {
+        // to_lowercase() 方法返回一个新的 String，而不是修改原来的 String
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
         }
-
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-
-        Ok(Config { query, file_path })
     }
-}
 
+    results
+}
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -62,5 +88,19 @@ safe, fast, productive.
         ";
 
         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
     }
 }
