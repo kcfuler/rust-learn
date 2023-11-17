4c0a7fa9424affcc691ad3f092b3696bef6404f3
kcfuler <kcfuler@gmail.com>
2023-11-17 21:59:18 +0800

    feat: finish auto-pointer

---
+++ advanced-concept/auto-pointer/rc.rs
@@ -0,0 +1,13 @@
+enum List {
+    Cons(i32, Rc<List>),
+    Nil,
+}
+
+use crate::List::{Cons, Nil};
+use std::rc::Rc;
+
+fn main() {
+    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))))
+    let b = Rc::clone(&a);
+    let c = Rc::clone(&a);
+}
